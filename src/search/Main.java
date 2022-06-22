5ea8aabb408f5765d738262c36638008fb9151d8
Indytskyi <ind.artem2003@gmail.com>
2022-06-22 16:14:58 +0300

    Start

---
+++ src/search/Main.java
@@ -0,0 +1,12 @@
+package search;
+
+import search.logic.Menu;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.getFromFileDataOfPeople();
+    }
+}
